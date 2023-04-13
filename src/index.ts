161977d97230a62c86f6f04f60b0ad6df1401a87
ErickEpnf18 <aguilakrakatoa@gmail.com>
2023-04-12 19:26:01 -0500

    part environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,19 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import  {resolvers}  from './resolvers/resolvers.js';
-import { typeDefs } from './typedefs/typedefs.js';
-import * as dotenv from 'dotenv';
-import { connectDB } from './db/config.js';
-dotenv.config()
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { resolvers } from "./resolvers/resolvers.js";
+import { typeDefs } from "./typedefs/typedefs.js";
+import { connectDB } from "./db/config.js";
+import { PORT } from "./utils/config.js";
 
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-  });
-  connectDB();
-  
+  typeDefs,
+  resolvers,
+});
+connectDB();
+
 const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-  });
-  
+  listen: { port: PORT },
+});
+
 console.log(`🚀  Server ready at: ${url}`);
